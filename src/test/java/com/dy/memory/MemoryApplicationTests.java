24e626c748102f57f759bcdb847b996d8927e627
Dayu <l1278945971@163.com>
2019-11-11 22:49:01 +0800

    the first commit

---
+++ src/test/java/com/dy/memory/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.memory;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
